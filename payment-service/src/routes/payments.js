536339ac14807b54c3f6b6bdaa16a69f918b4a7a
Lorne Stalker <lorne.stalker@gmail.com>
2023-07-12 19:05:13 +0100

    cutting delay times slightly

--- payment-service/src/routes/payments.js
+++ payment-service/src/routes/payments.js
@@ -29,12 +29,12 @@ async function processPayment(req, res) {
 function validateCardNumber(cardNumber) {
   // here we're just hardcoding a response based on the mock users. One card is valid the other is not
   // we're also simulating a network delay of up to a 2 seconds
-  return delay(cardNumber === "****-****-****-9876", Math.random() * 2)
+  return delay(cardNumber === "****-****-****-9876", Math.random() * 1)
 }
 
 async function processWithThirdPartyPaymentProvider() {
   // here we're simulating a call to a third party payment provider, with potential network delays up to 6 seconds.
-  return delay(Math.random() < 0.5, Math.random() * 6)
+  return delay(Math.random() < 0.5, Math.random() * 3)
 }
 
 export {getPaymentRoutes}
